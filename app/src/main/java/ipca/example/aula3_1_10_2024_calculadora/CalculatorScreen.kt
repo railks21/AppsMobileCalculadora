067c0e1ddf691105ba71e111efb81306402eba52
railks21 <josepedro.abreu21@gmail.com>
2024-10-01 10:31:51 +0100

    First Commit

---
+++ app/src/main/java/ipca/example/aula3_1_10_2024_calculadora/CalculatorScreen.kt
@@ -0,0 +1,2 @@
+package ipca.example.aula3_1_10_2024_calculadora
+
---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "aula3_1_10_2024_Calculadora"
+include(":app")
