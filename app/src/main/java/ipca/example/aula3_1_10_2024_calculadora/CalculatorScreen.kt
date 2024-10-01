fa995537792465d11c71a633433f2962eced763c
railks21 <josepedro.abreu21@gmail.com>
2024-10-01 10:52:44 +0100

    Calculator

--- app/src/main/java/ipca/example/aula3_1_10_2024_calculadora/CalculatorScreen.kt
+++ app/src/main/java/ipca/example/aula3_1_10_2024_calculadora/CalculatorScreen.kt
@@ -1,2 +1,50 @@
 package ipca.example.aula3_1_10_2024_calculadora
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun CalculatorScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "0")
+        Row {
+            Button(onClick = { /* ToDo */ } ) {
+                Text(text = "7")
+            }
+            Button(onClick = { /* ToDo */ } ) {
+                Text(text = "8")
+            }
+            Button(onClick = { /* ToDo */ } ) {
+                Text(text = "9")
+            }
+        }
+        Row {
+            Button(onClick = { /* ToDo */ } ) {
+                Text(text = "4")
+            }
+            Button(onClick = { /* ToDo */ } ) {
+                Text(text = "5")
+            }
+            Button(onClick = { /* ToDo */ } ) {
+                Text(text = "6")
+            }
+        }
+        Row {
+            Button(onClick = { /* ToDo */ } ) {
+                Text(text = "1")
+            }
+            Button(onClick = { /* ToDo */ } ) {
+                Text(text = "2")
+            }
+            Button(onClick = { /* ToDo */ } ) {
+                Text(text = "3")
+            }
+        }
+    }
+}
--- app/src/main/java/ipca/example/aula3_1_10_2024_calculadora/MainActivity.kt
+++ app/src/main/java/ipca/example/aula3_1_10_2024_calculadora/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import ipca.example.aula3_1_10_2024_calculadora.ui.theme.Aula3_1_10_2024_CalculadoraTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +17,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             Aula3_1_10_2024_CalculadoraTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CalculatorScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Aula3_1_10_2024_CalculadoraTheme {
-        Greeting("Android")
     }
 }
